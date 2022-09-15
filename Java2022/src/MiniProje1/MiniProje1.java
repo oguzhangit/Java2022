f8228a749d4819d1ccd5f76ba9a367e5c617e136
Oğuzhan <Ouzhan@oguzhan>
2022-09-15 15:20:36 +0300

    MiniProje1

--- Java2022/src/MiniProje1/MiniProje1.java
+++ Java2022/src/MiniProje1/MiniProje1.java
@@ -5,10 +5,11 @@ package MiniProje1;
 public class MiniProje1 {
 	
 	public static void main(String[] args) {
-		int number= 13;
+		int number= 2;
 		double remainder;
 		
-		if(number<2) {System.err.println("Geçersiz Sayı");}
+		if(number<2) {System.out.println("Geçersiz Sayı"); return; }
+		if(number == 1) {System.out.println("Asal Değil");return;}
 		
 		boolean isPrime = true;
 		for (int i = 2; i < number; i++) {
