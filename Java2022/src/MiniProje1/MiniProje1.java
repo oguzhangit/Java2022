9643984df41c898f1e4fc13c25a73b501fcfcef8
Oğuzhan <Ouzhan@oguzhan>
2022-09-15 15:18:18 +0300

    MiniProje

--- Java2022/src/MiniProje1/MiniProje1.java
+++ Java2022/src/MiniProje1/MiniProje1.java
@@ -7,7 +7,9 @@ public class MiniProje1 {
 	public static void main(String[] args) {
 		int number= 13;
 		double remainder;
-		 
+		
+		if(number<2) {System.err.println("Geçersiz Sayı");}
+		
 		boolean isPrime = true;
 		for (int i = 2; i < number; i++) {
 			
