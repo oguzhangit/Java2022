0a6205b2a9bdcdda47265a68e891a86fe11a934b
Oğuzhan <Ouzhan@oguzhan>
2022-09-15 14:51:15 +0300

    RecapDemo1

--- Java2022/src/RecapDemo1/RecapDemo1.java
+++ Java2022/src/RecapDemo1/RecapDemo1.java
@@ -5,6 +5,17 @@ public class RecapDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sayi1 = 50;
+		int sayi2 = 60;
+		int sayi3 = 5;
+		int enBuyuk = sayi1;
+		
+		if (enBuyuk < sayi2) {
+			enBuyuk = sayi2;
+		}else if(enBuyuk<sayi3) {
+			enBuyuk = sayi3;
+		}
+		
+		System.out.println("En büyük =  " + enBuyuk);
 	}
 
 }
